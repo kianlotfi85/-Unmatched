8e0bf53f58a25debd65cb216f327fb8a0586cb4e
kian Lotfi <lotfikian85@gmail.com>
2026-06-28 00:36:45 +0330

    zones and Space added

--- include/Space.hpp
+++ include/Space.hpp
@@ -1,15 +1,16 @@
 #ifndef SPACE_H
 #define SPACE_H
+#include "enums.hpp"
 #include <vector>
-class Space
-{
+class Space {
 public:
 
-private:
-    int id; // the id of the Space
-    std::vector<int> neighbors;
-    
-};
 
+private:
+  int id;                     // the id of the Space
+  std::vector<Zone> zones;    // the zones that Space is part of them
+  std::vector<int> neighbors; // Vector of Space neighbors The number of
+                              // neighbors is not fixed.
+};
 
 #endif
