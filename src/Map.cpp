5f74eb89d3720e45f4e1e65b81d9c66e75db6ca3
kian Lotfi <lotfikian85@gmail.com>
2026-07-02 16:05:11 +0330

    Start implementing Map src

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,1 +1,26 @@
 #include "../include/Map.hpp"
+
+Map::Map()
+{
+
+}
+
+bool Map::canMove( int from, int to ) const
+{
+    return true;
+}
+
+std::vector<int> Map::getReachableSpaces( int fromID, int maxMoves)
+{
+    return {};
+}
+
+int Map::getDistance(int from, int to) const
+{
+    return 0;
+}
+
+void Map::moveFighter(int from, int to) const
+{
+    
+}
