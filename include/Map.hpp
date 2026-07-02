136ea20637ba93fcec182ce3723c56e7eb367b69
kian Lotfi <lotfikian85@gmail.com>
2026-07-02 23:16:42 +0330

    Map constructor and intialize Space private method added

--- include/Map.hpp
+++ include/Map.hpp
@@ -7,13 +7,14 @@
 class Map
 {
 public:
-    Map();
+    Map(); // Note : Map constructor --> need? list of Spaces - list of secretPassages
     bool canMove( int from, int to ) const;
     std::vector<int> getReachableSpaces( int fromID, int maxMoves); // BFS algorithm
     int getDistance(int from, int to) const;
     void moveFighter(int from, int to) const;
+    
 private:
-
+    void initializeSpaces(); // Helper function
     std::vector<Space> spaces;
     std::vector<int> secretPassageSpaces; // List of secretPassages
 
